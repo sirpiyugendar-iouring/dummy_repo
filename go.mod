cdcd326d8eae87359f592b20c4ad111bc45e343b
Sirpi Yugendar Rajendran <sirpi.yugendar@iouring.com>
2025-05-16 09:31:41 +0530

    Revert "empty go files"
    This reverts commit c88816d77a5c4e48a1ff593021822f34d05d38e0.
    
    	modified:   go.mod
    	new file:   go.sum
    	new file:   jp.go
    	modified:   main.go

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module dummy
 
 go 1.21.1
+
+require github.com/go-redis/redis v6.15.9+incompatible
+
+require (
+	github.com/onsi/ginkgo v1.16.5 // indirect
+	github.com/onsi/gomega v1.18.1 // indirect
+)
--- main.go
+++ main.go
@@ -2,8 +2,19 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/go-redis/redis"
 )
 
 func main() {
-	fmt.Println()
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+	ch := rdb.Subscribe("new").Channel()
+	for {
+		msg := <-ch
+		fmt.Println(msg.Payload)
+	}
 }
