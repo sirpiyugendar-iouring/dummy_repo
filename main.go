538dfa7774ac8ee7ae685814b8ae9fc62c701b00
Sirpi Yugendar Rajendran <sirpi.yugendar@iouring.com>
2025-05-16 10:57:50 +0530

    Revert: revert

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ var Cache atomic.Value
 var keys []string
 
 func main() {
-    keys = strings.Split("1 2 3 4 5 6 7 8 9 10", " ")
+	keys = strings.Split("1 2 3 4 5 6 7 8 9 10", " ")
 	m = make(map[string]int)
 	Cache.Store(m)
 	for i := 0; i < 10; i++ {
@@ -36,8 +36,8 @@ func main() {
 
 func newMap(id int) map[string]int {
 	res := map[string]int{}
-    for _, key := range keys {
-	    res[key] = id
-    }
+	for _, key := range keys {
+		res[key] = id
+	}
 	return res
 }
