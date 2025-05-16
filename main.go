4970ac5cc2c4bed602903255f961cc717c1d6a0e
Sirpi Yugendar Rajendran <sirpi.yugendar@iouring.com>
2025-05-16 10:56:06 +0530

    Revert "atomic.Value check"
    This reverts commit 874b6f4b93fd5688f074226990c0857c85bbcea2.

--- main.go
+++ main.go
@@ -2,42 +2,19 @@ package main
 
 import (
 	"fmt"
-	"strings"
-	"sync/atomic"
-	"time"
+
+	"github.com/go-redis/redis"
 )
 
-var m map[string]int
-var Cache atomic.Value
-var keys []string
-
 func main() {
-    keys = strings.Split("1 2 3 4 5 6 7 8 9 10", " ")
-	m = make(map[string]int)
-	Cache.Store(m)
-	for i := 0; i < 10; i++ {
-		i := i
-		go func(val int) {
-			for {
-				Map := newMap(val)
-				Cache.Store(Map)
-			}
-		}(i)
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+	ch := rdb.Subscribe("new").Channel()
+	for {
+		msg := <-ch
+		fmt.Println(msg.Payload)
 	}
-	go func() {
-		for {
-			time.Sleep(time.Microsecond)
-			c := Cache.Load().(map[string]int)
-			fmt.Println(c)
-		}
-	}()
-	time.Sleep(time.Minute / 4)
-}
-
-func newMap(id int) map[string]int {
-	res := map[string]int{}
-    for _, key := range keys {
-	    res[key] = id
-    }
-	return res
 }
